3ad2297badcf63de0b58068dc22964b96839e19c
miri <m0534106023@Gmail.com>
2025-04-27 18:05:36 +0300

    Initial commit

---
+++ הוספת נתוני הבחירות של בחירות לממשלה 2022.sql
@@ -0,0 +1,121 @@
+
+truncate table envelope
+
+declare @vode int = 1115336
+while(@vode>0)
+begin
+exec addEnvelope null,'מחל'
+set @vode = @vode-1
+end
+
+declare @vode int
+set @vode = 847435
+while(@vode>0)
+begin
+exec addEnvelope null,'פה'
+set @vode = @vode-1
+end
+
+declare @vode int
+set @vode = 516470
+while(@vode>0)
+begin
+exec addEnvelope null,'ט'
+set @vode = @vode-1
+end
+
+declare @vode int
+set @vode = 432482
+while(@vode>0)
+begin
+exec addEnvelope null,'כן'
+set @vode = @vode-1
+end
+
+declare @vode int
+set @vode = 392964
+while(@vode>0)
+begin
+exec addEnvelope null,'שס'
+set @vode = @vode-1
+end
+
+declare @vode int
+set @vode = 280194
+while(@vode>0)
+begin
+exec addEnvelope null,'ג'
+set @vode = @vode-1
+end
+
+declare @vode int
+set @vode = 213687
+while(@vode>0)
+begin
+exec addEnvelope null,'ל'
+set @vode = @vode-1
+end
+
+declare @vode int
+set @vode = 194047
+while(@vode>0)
+begin
+exec addEnvelope null,'עם'
+set @vode = @vode-1
+end
+
+declare @vode int
+set @vode = 178735
+while(@vode>0)
+begin
+exec addEnvelope null,'ום'
+set @vode = @vode-1
+end
+
+declare @vode int
+set @vode = 175992
+while(@vode>0)
+begin
+exec addEnvelope null,'אמת'
+set @vode = @vode-1
+end
+
+declare @vode int
+set @vode = 150793
+while(@vode>0)
+begin
+exec addEnvelope null,'מרצ'
+set @vode = @vode-1
+end
+
+declare @vode int
+set @vode = 8800
+while(@vode>0)
+begin
+exec addEnvelope null,'צב'
+set @vode = @vode-1
+end
+
+declare @vode int
+set @vode = 87065
+while(@vode>0)
+begin
+exec addEnvelope null,'ב'
+set @vode = @vode-1
+end
+
+declare @vode int
+set @vode = 10000
+while(@vode>0)
+begin
+exec addEnvelope null,'x'
+set @vode = @vode-1
+end
+
+declare @vode int
+set @vode = 10000
+while(@vode>0)
+begin
+exec addEnvelope 1,'שס'
+set @vode = @vode-1
+end
